4157a3535f3bcb708f5e600e4ade4573ef4dffc8
Brian Mearns <bmearns@ieee.org>
2015-05-28 21:15:54 -0400

    Rendering an agenda as a simle list of Tasks.

--- agenda.jsx
+++ agenda.jsx
@@ -1,13 +1,40 @@
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
+
+var Task = React.createClass({
+    render: function() {
+        return (
+            <div
+                className='task'
+            >
+                <h2 className='_title'>{this.props.title}</h2>
+                <h3 className='_duration'>{this.props.duration}</h3>
+            </div>
+        );
+    },
 });
+
+
+var Agenda = React.createClass({
+    render: function() {
+        var childIdx = 0;
+        return (
+            <ul>
+                {this.props.children.map(function(child) {
+                    var key = childIdx;
+                    childIdx++;
+                    return (
+                        <li key={key}>{child}</li>
+                    );
+                })}
+            </ul>
+        );
+    },
+});
+
 React.render(
-  <CommentBox />,
+  <Agenda>
+    <Task duration='10' title='First Task' />
+    <Task duration='20' title='Second Task' />
+    <Task duration='8' title='Third Task' />
+  </Agenda>,
   document.body
 );
