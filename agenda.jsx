c0a5bf30e8e6e310277dc28db156dea09240d74e
Brian Mearns <bmearns@ieee.org>
2015-05-28 21:39:48 -0400

    Relatively sized tasks. More or less.

--- agenda.jsx
+++ agenda.jsx
@@ -4,10 +4,12 @@ var Task = React.createClass({
         return (
             <div
                 className='task'
+                style={{
+                    height: this.props.height + 'px',
+                }}
             >
                 <h2 className='_title'>{this.props.title}</h2>
                 <h3 className='_duration'>{this.props.duration}</h3>
-                <h3 className='_pct'>{this.props.pct}</h3>
             </div>
         );
     },
@@ -15,19 +17,27 @@ var Task = React.createClass({
 
 
 var Agenda = React.createClass({
+    getDefaultProps: function() {
+        return {
+            height: 300,
+        }
+    },
+
     render: function() {
         var childIdx = 0;
         var total = 0.0;
         this.props.children.forEach(function(child) {
             total += parseFloat(child.props.duration);
         });
+        var height = this.props.height;
         return (
             <ul className='agenda'>
                 {this.props.children.map(function(child) {
                     var key = childIdx;
                     childIdx++;
+                    var pct = parseFloat(child.props.duration) / total;
                     return (
-                        <li key={key}><Task {...child.props} pct={100.0*parseFloat(child.props.duration) / total} /></li>
+                        <li key={key} ><Task {...child.props} height={height * pct} /></li>
                     );
                 })}
             </ul>
