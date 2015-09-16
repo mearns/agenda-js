9c5fde976c6e591f82ad7aeb59c0027b0eb5cdeb
Brian Mearns <bmearns@ieee.org>
2015-09-16 20:49:43 +0100

    It's ugly, but it's so much simpler.

--- agenda.js
+++ agenda.js
@@ -1,8 +1,8 @@
 
 var agenda = [
-    {duration: 10, title: "First Task"},
-    {duration: 20, title: "Second Task"},
-    {duration: 8, title: "Third Task"},
+    {duration: 3, title: "First Task"},
+    {duration: 6, title: "Second Task"},
+    {duration: 2, title: "Third Task"},
 ];
 
 var onload = function() {
@@ -15,17 +15,54 @@ var onload = function() {
         totalDuration += agenda[i].duration;
     }
 
-    for(var i=0; i<agenda.length; i++) {
-        var task = agenda[i];
-        var taskBuilder =
-            new HtmlBuilder(null, "div")
-                .addClass("Task")
-                .style("height", (totalHeight * (task.duration / totalDuration)) + "px")
-                .ele("h2").text(task.title).up()
-                .ele("h3").text(task.duration).up()
+    var startTime = new Date();
+    var currentIdx = 0;
 
-        agendaEle.appendChild(taskBuilder.build());
+    function tick() {
+
+        var elapsedTime = parseFloat((new Date()) - startTime) / 1000.0;
+        var remainingTime = elapsedTime;
+
+        agendaEle.innerHTML = "";
+        for(var i=0; i<agenda.length; i++) {
+            var task = agenda[i];
+
+            var pct = 0;
+            if(i == currentIdx) {
+                pct = remainingTime / task.duration;
+                if(pct >= 1.0) {
+                    pct = 1.0;
+                    currentIdx++;
+                }
+                remainingTime = 0;
+            } else if(i < currentIdx) {
+                pct = 1.0;
+                remainingTime -= task.duration;
+            } else {
+                pct = 0.0;
+            }
+            pct *= 100.0;
+
+            var taskBuilder =
+                new HtmlBuilder(null, "div")
+                    .addClass("Task")
+                    .style("height", (totalHeight * (task.duration / totalDuration)) + "px")
+                    .ele("div")
+                        .addClass("VerticalProgressBar")
+                        .ele("div").addClass("_prog").style("height", pct + "%").up()
+                    .up()
+                    .ele("h2").text(task.title).up()
+                    .ele("h3").text(task.duration).up()
+
+            agendaEle.appendChild(taskBuilder.build());
+        }
+
+        if(currentIdx >= agenda.length) {
+            clearInterval(timer);
+        }
     }
+
+    var timer = setInterval(tick, 50);
 
 };
 
